9a9e93909ffadf09e02d8de9141dfc7e0ece3ea9
Bantea Carmen <bantea.carmen93@gmail.com>
2021-05-14 18:28:54 +0300

    OA-6 update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
+
         System.out.println("test");
         System.out.println("second test");
         System.out.println("test 3");
-=======
+
+
+        System.out.println("test 4");
+
         System.out.println("test ....code in jira???");
->>>>>>> 541c4bde60422901d35f7c0c3a7aab508f25ab80
+
     }
 }
 
